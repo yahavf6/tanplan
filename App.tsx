fdd584d3a7ad91139257737c31c329d196f30ec5
Yahav Fuchs <yahavf@realfriend.ai>
2020-01-09 12:03:40 +0200

    git init

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <
+      <Text>Open up App.tsx to start working on your app! Yahav</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column'
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
